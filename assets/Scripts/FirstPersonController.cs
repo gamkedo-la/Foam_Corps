639bd3a5a9b15fba8c2797894f9e34b54b56a858
MaesterLazer <daniel.l.lazar@gmail.com>
2015-10-23 12:17:17 -0700

    V1.01 - Props etc.
    -Added prefab package for decorative props (in Resources)
    -Added speedCoolTime to FPS Controller
    -changed speedCoolTime to 10 sec
    -changed SpeedPowerUp speed to 6
    -changed default speeds to walk 3, run 5
    -made folders in Resources (Characters, PowerUps, Props)
    -moved Spawn Points and Power Up in scene to area “Cubicles” for better
    testing

--- assets/Scripts/FirstPersonController.cs
+++ assets/Scripts/FirstPersonController.cs
@@ -28,6 +28,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
         [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
         [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+		[SerializeField] private float speedCoolTime;
 
         private Camera m_Camera;
         private bool m_Jump;
@@ -110,7 +111,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 		}
 
 		IEnumerator SpeedPowerDown() {
-			yield return new WaitForSeconds (15);
+			yield return new WaitForSeconds (speedCoolTime);
 			m_WalkSpeed = defaultWalkSpeed;
 		}
 
