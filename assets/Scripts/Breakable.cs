a9e82b4c4242e0fa1bc9d971399e39401e1fad39
MaesterLazer <daniel.l.lazar@gmail.com>
2015-10-27 09:33:57 -0700

    Doors and Tags
    -restored tags and layers (hopefully they stay)
    -Added breakable doors
    -currently break on collision
    -colliders could work better, changed to OnTriggerEnter instead of
    OnCollisionEnter and added a wide box collider trigger to help. It made
    it SLIGHTLY better. Should implement non-collision triggering in the
    future (melee raycast?)

---
+++ assets/Scripts/Breakable.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Breakable : MonoBehaviour {
+
+public MeshCollider[] piece;
+public Rigidbody[] rigidbody;
+public int numberOfPieces;
+
+	// Use this for initialization
+	void Start () {
+	
+		for(int i = 0; i < numberOfPieces; i++){
+			Transform c = this.transform.GetChild(i);
+			piece[i] = c.GetComponent<MeshCollider>();
+			rigidbody[i] = c.GetComponent<Rigidbody>();
+
+			
+		   
+		   //foreach (MeshCollider piece in pieces){
+           //piece.Value = GameObject.GetComponentInChildren<MeshCollider>();
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		for(int i = 0; i < numberOfPieces; i++){
+			rigidbody[i].isKinematic = false;
+		}
+	}
+	
+	//FOR TESTING
+		// void Update () {
+		//	if(Input.GetKeyDown("space")) {
+			//	for(int i = 0; i < numberOfPieces; i++){
+			//	rigidbody[i].isKinematic = false;}}
+
+	
+
+}
