6d9a21041b7758fd8db55497dc000696ef29c065
Chris DeLeon <gamkedo@gmail.com>
2015-10-26 23:44:24 -0700

    Fog off while editing, turns on when starting
    Made a new one-line script for this purpose, it currently lives on the
    networkmanager

---
+++ assets/FogTurnOn.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogTurnOn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("fog turned on by script (off in editor for visibility)");
+		RenderSettings.fog = true;
+	}
+}
