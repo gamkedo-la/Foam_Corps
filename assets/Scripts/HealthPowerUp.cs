52ea9b7ae8d3e789005b7555455c1d74b40ef3de
Jeremy Kenyon <lanthos@gmail.com>
2015-11-01 14:55:42 -0800

    Power up rotations
    Updated the colliders on the power-ups to be box colliders instead of
    capsule.  Probably not a huge difference but my OCD really likes it a
    lot better cause it fits the object MUCH better.  Also added a rotate
    option to the script so that the power-ups are rotating.  Need to
    figure why the one for the cross is off center though.

--- assets/Scripts/HealthPowerUp.cs
+++ assets/Scripts/HealthPowerUp.cs
@@ -4,10 +4,16 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class HealthPowerUp : MonoBehaviour {
 
+	public float rotateSpeed = 2.0f;
+
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.GetComponent<FirstPersonController> () != null) {
 			other.gameObject.GetComponent<FirstPersonController> ().HealPowerUp();
 			Destroy(gameObject);
 		}
+	}
+
+	void Update(){
+		transform.Rotate (Vector3.up, rotateSpeed);
 	}
 }
--- assets/Scripts/SpeedPowerUp.cs
+++ assets/Scripts/SpeedPowerUp.cs
@@ -5,11 +5,16 @@ using UnityStandardAssets.Characters.FirstPerson;
 public class SpeedPowerUp : MonoBehaviour {
 
 	public float speed = 7;
+	public float rotateSpeed = 2.0f;
 
 	void OnTriggerEnter (Collider other) {
 		if (other.gameObject.GetComponent<FirstPersonController> () != null) {
 			other.gameObject.GetComponent<FirstPersonController> ().SpeedPowerUp(speed);
 			Destroy(gameObject);
 		}
-	}	
+	}
+
+	void Update(){
+		transform.Rotate (Vector3.up, rotateSpeed);
+	}
 }
