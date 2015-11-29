6d858194d739d30f96c4e3585285d5c9f0215e00
Chris DeLeon <gamkedo@gmail.com>
2015-11-29 01:04:11 -0800

    Prop colors and wall color tweaked, camera clip fixed (wider player + nearer near clipping plane), added box colliders on 2 bigscreens that were missing them, added sound script but haven't added sounds yet

---
+++ assets/Scripts/SoundCenter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundCenter : MonoBehaviour {
+	public static SoundCenter instance;
+
+	public AudioClip sodaSplash;
+	public AudioClip watergunSquirt;
+	public AudioClip zombieMoan;
+	public AudioClip doorBreak;
+	public AudioClip melee;
+	public AudioClip dartShoot;
+	public AudioClip dartStick;
+	public AudioClip playerHurt;
+	public AudioClip playerDie;
+
+	void Awake() {
+		if(instance) {
+			Destroy(instance.gameObject);
+		}
+		instance = this;
+	}
+
+	public void PlayClipOn(AudioClip clip, Vector3 pos, float atVol = 1.0f,
+	                       Transform attachToParent = null) {
+		GameObject tempGO = new GameObject("TempAudio"); // create the temp object
+		tempGO.transform.position = pos; // set its position
+		if(attachToParent != null) {
+			tempGO.transform.parent = attachToParent.transform;
+		}
+		AudioSource aSource = tempGO.AddComponent<AudioSource>() as AudioSource; // add an audio source
+		aSource.clip = clip; // define the clip
+		aSource.volume = atVol;
+		aSource.pitch = Random.Range(0.85f,1.15f);
+		// set other aSource properties here, if desired
+		aSource.Play(); // start the sound
+		Destroy(tempGO, clip.length/aSource.pitch); // destroy object after clip duration
+	}
+}
