59975d884c4969a41a9a62c7cfb15e5c5d533d1c
Chris DeLeon <gamkedo@gmail.com>
2015-12-01 04:39:49 -0800

    Title screen with buttons for Single Player, Multiplayer, Credits
    Made environment into a prefab for sake of keeping opening stage area
    in sync with any changes to it from the game scene

---
+++ assets/MainMenuFunctions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuFunctions : MonoBehaviour {
+	public GameObject creditsCanvas;
+	public GameObject mainButtonsCanvas;
+
+	public void StartSingleplayer() {
+		PhotonNetwork.PhotonServerSettings.HostType = ServerSettings.HostingOption.OfflineMode;
+		Application.LoadLevel("LEVEL_Cubicles");
+	}
+
+	public void StartMultiplayer() {
+		PhotonNetwork.PhotonServerSettings.HostType = ServerSettings.HostingOption.PhotonCloud;
+		Application.LoadLevel("LEVEL_Cubicles");
+	}
+
+	public void CreditsToggle() {
+		creditsCanvas.SetActive( mainButtonsCanvas.activeSelf );
+		mainButtonsCanvas.SetActive( mainButtonsCanvas.activeSelf == false );
+	}
+}
--- assets/Scripts/Spin.cs
+++ assets/Scripts/Spin.cs
@@ -2,9 +2,10 @@
 using System.Collections;
 
 public class Spin : MonoBehaviour {
+	public float spinAmt = 40.0f;
 
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(Vector3.up,Time.deltaTime * 40.0f);
+		transform.Rotate(Vector3.up,Time.deltaTime * spinAmt);
 	}
 }
